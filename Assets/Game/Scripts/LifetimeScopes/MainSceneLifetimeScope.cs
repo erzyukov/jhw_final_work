00f64901ec4c68b5a311253628b92e502705fe51
Erzyukov <erzyukov@gmail.com>
2023-10-15 21:37:23 +1000

    base vcontainer settings

---
+++ Assets/Game/Scripts/LifetimeScopes/MainSceneLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Game
+{
+    public class MainSceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
