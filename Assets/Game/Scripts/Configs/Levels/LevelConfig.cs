db94a8921ca8f3a6209cbc62594ea8cc92dbe764
Erzyukov <erzyukov@gmail.com>
2023-11-15 08:13:40 +1000

    lobby screen view

--- Assets/Game/Scripts/Configs/Levels/LevelConfig.cs
+++ Assets/Game/Scripts/Configs/Levels/LevelConfig.cs
@@ -5,9 +5,11 @@ namespace Game.Configs
 	[CreateAssetMenu(fileName = "Level", menuName = "Configs/Level", order = (int)Config.Level)]
 	public class LevelConfig : ScriptableObject
 	{
+		[SerializeField] private string _title;
 		[SerializeField] private Region _region;
 		[SerializeField] private WaveConfig[] _waves;
 
+		public string Title => _title;
 		public Region Region => _region;
 		public WaveConfig[] Waves => _waves;
 	}
--- Assets/Game/Scripts/Ui/Screens/UiLobbyScreen.cs
+++ Assets/Game/Scripts/Ui/Screens/UiLobbyScreen.cs
@@ -1,7 +1,34 @@
 ﻿namespace Game.Ui
 {
-	public class UiLobbyScreen : UiScreen
+	using UnityEngine;
+	using TMPro;
+	using UnityEngine.UI;
+	using System;
+	using UniRx;
+
+	public interface IUiLobbyScreen
 	{
+		void SetTitle(string value);
+		void SetLastWaveTitle(string value);
+		IObservable<Unit> PlayButtonClicked { get; }
+	}
+
+	public class UiLobbyScreen : UiScreen, IUiLobbyScreen
+	{
+		[SerializeField] private TextMeshProUGUI _levelTitle;
+		[SerializeField] private TextMeshProUGUI _lastWaveTitle;
+		[SerializeField] private Button _playButton;
+
 		public override Screen Screen => Screen.Lobby;
+
+		#region IUiLobbyScreen
+
+		public IObservable<Unit> PlayButtonClicked => _playButton.OnClickAsObservable();
+		
+		public void SetTitle(string value) => _levelTitle.text = value;
+
+		public void SetLastWaveTitle(string value) => _lastWaveTitle.text = value;
+		
+		#endregion
 	}
 }
