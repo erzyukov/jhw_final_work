5aa85c8e7fc8139de26621d5c2a7a24013dc77fe
Erzyukov <erzyukov@gmail.com>
2024-01-02 18:06:12 +1000

    refact attaker; create UnitAttackerBase

---
+++ Assets/Game/Scripts/Level/Units/Components/Attacker/UnitAttackerBase.cs
@@ -0,0 +1,47 @@
+﻿namespace Game.Units
+{
+	using Game.Utilities;
+	using Zenject;
+	using Game.Configs;
+	using UnityEngine;
+	using Game.Core;
+
+	public interface IUnitAttacker
+	{
+		bool IsReadyToAttack { get; }
+		bool IsTargetClose(IUnitFacade target);
+		void Attack(IUnitFacade target);
+	}
+
+	public abstract class UnitAttackerBase : ControllerBase, IInitializable, IUnitAttacker
+	{
+		[Inject] IUnitView _view;
+		[Inject] private Species _species;
+		[Inject] private IGameUpgrades _gameUpgrades;
+		[Inject] UnitGrade _grade;
+		[Inject] UnitConfig _config;
+
+		protected ITimer AtackTimer;
+		protected float CurrentDamage;
+
+		public virtual void Initialize()
+		{
+			AtackTimer = new Timer();
+			CurrentDamage = _gameUpgrades.GetUnitDamage(_species) * _grade.DamageMultiplier;
+		}
+
+		#region IUnitAttacker
+
+		public bool IsReadyToAttack => AtackTimer.IsReady;
+
+		public abstract void Attack(IUnitFacade target);
+
+		public bool IsTargetClose(IUnitFacade target) =>
+			target != null &&
+			target.IsDead == false &&
+			(_view.Transform.position - target.Transform.position).sqrMagnitude <
+			_config.AttackRange * _config.AttackRange + Mathf.Epsilon;
+
+		#endregion
+	}
+}
--- Assets/Game/Scripts/Level/Units/Components/UnitAttacker.cs
+++ Assets/Game/Scripts/Level/Units/Components/UnitAttacker.cs
@@ -1,52 +1,18 @@
 ﻿namespace Game.Units
 {
-	using Game.Utilities;
 	using Zenject;
-	using Game.Configs;
-	using UnityEngine;
-	using Game.Core;
 
-	public interface IUnitAttacker
+	public class UnitAttacker : UnitAttackerBase, IInitializable, IUnitAttacker
 	{
-		bool IsReadyToAttack { get; }
-		bool IsTargetClose(IUnitFacade target);
-        void Attack(IUnitFacade target);
-	}
-
-    // TODO: Add base attacker class
-
-	public class UnitAttacker : ControllerBase, IInitializable, IUnitAttacker
-	{
-		[Inject] IUnitView _view;
-		[Inject] private Species _species;
-		[Inject] private IGameUpgrades _gameUpgrades;
 		[Inject] UnitGrade _grade;
-		[Inject] UnitConfig _config;
-
-		private ITimer _atackTimer;
-		private float _currentDamage;
-
-		public void Initialize()
-		{
-			_atackTimer = new Timer();
-			_currentDamage = _gameUpgrades.GetUnitDamage(_species) * _grade.DamageMultiplier;
-		}
 
 		#region IUnitAttacker
 
-		public bool IsReadyToAttack => _atackTimer.IsReady;
-
-        public void Attack(IUnitFacade target)
+        public override void Attack(IUnitFacade target)
         {
-			target.TakeDamage(_currentDamage);
-            _atackTimer.Set(_grade.AttackDelay);
+			target.TakeDamage(CurrentDamage);
+            AtackTimer.Set(_grade.AttackDelay);
         }
-
-		public bool IsTargetClose(IUnitFacade target) =>
-            target != null && 
-            target.IsDead == false &&
-            (_view.Transform.position - target.Transform.position).sqrMagnitude < 
-			_config.AttackRange * _config.AttackRange + Mathf.Epsilon;
 
 		#endregion
 	}
