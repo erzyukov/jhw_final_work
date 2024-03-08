a7437baeea5853813b6a5ee892ec27a70c957659
Erzyukov <erzyukov@gmail.com>
2024-03-09 09:00:10 +1000

    prefabs config

---
+++ Assets/Game/Scripts/Configs/Technical/PrefabsConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace Game.Configs
+{
+	using Game.Fx;
+	using Sirenix.OdinInspector;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	[CreateAssetMenu(fileName = "Prefabs", menuName = "Configs/Prefabs", order = (int)Config.Prefabs)]
+	public class PrefabsConfig : SerializedScriptableObject
+	{
+		[Header("Bullet / Technical")]
+		[SerializeField] private Dictionary<VfxElement, PooledParticleFx> _vfxPrefabs = new Dictionary<VfxElement, PooledParticleFx>();
+
+		public Dictionary<VfxElement, PooledParticleFx> VfxPrefabs => _vfxPrefabs;
+
+		public PooledParticleFx GetVfx(VfxElement type)
+		{
+			if (_vfxPrefabs.TryGetValue(type, out PooledParticleFx element))
+				return element;
+
+			return null;
+		}
+	}
+}
