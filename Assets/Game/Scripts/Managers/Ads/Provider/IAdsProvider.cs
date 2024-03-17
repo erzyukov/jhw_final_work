1a90a818ec8763f56fa78d3d13487854ca7a609b
Erzyukov <erzyukov@gmail.com>
2024-03-17 12:42:07 +1000

    YandexAdsProvider

--- Assets/Game/Scripts/Managers/Ads/Provider/IAdsProvider.cs
+++ Assets/Game/Scripts/Managers/Ads/Provider/IAdsProvider.cs
@@ -5,18 +5,18 @@
 	public interface IAdsProvider
 	{
 		ReactiveCommand Initialized { get; }
-		
+
 		ReactiveCommand<EAdType> AdLoaded { get; }
 		ReactiveCommand<EAdType> AdOpened { get; }
 		ReactiveCommand<EAdType> AdClosed { get; }
 		ReactiveCommand<EAdType> AdShowFailed { get; }
-		
-		ReactiveCommand Rewarded { get; }
-		
+
+		ReactiveCommand<ERewardedType> Rewarded { get; }
+
 		bool IsAdAvailable( EAdType type );
-		void ShowInterstitialVideo(string place);
-		void ShowRevardedVideo(string place);
-		void DisplayBanner(string place);
+		void ShowInterstitialVideo( string place );
+		void ShowRevardedVideo( ERewardedType type );
+		void DisplayBanner( string place );
 		void HideBanner();
 	}
 }
---
+++ Assets/Game/Scripts/Managers/Ads/Provider/YandexAdsProvider.cs
@@ -0,0 +1,99 @@
+﻿namespace Game.Managers
+{
+	using System;
+	using UniRx;
+	using Zenject;
+	using YG;
+	using Game.Utilities;
+
+	public class YandexAdsProvider : ControllerBase, IAdsProvider, IInitializable
+	{
+		public void Initialize()
+		{
+			InitializeSubscribe();
+			InterstitialSubscribe();
+			RewardedVideoSubscribe();
+		}
+
+		private void RewardedVideoSubscribe()
+		{
+			ObserveEvent( YandexGame.OpenVideoEvent, () => AdOpened.Execute( EAdType.RewardedVideo ) );
+			ObserveEvent( YandexGame.ErrorVideoEvent, () => AdShowFailed.Execute( EAdType.RewardedVideo ) );
+			ObserveEvent( YandexGame.CloseVideoEvent, () =>
+			{
+				AdClosed.Execute( EAdType.RewardedVideo );
+				AdLoaded.Execute( EAdType.RewardedVideo );
+			} );
+
+			Observable.FromEvent<int>(
+					x => YandexGame.RewardVideoEvent += x,
+					x => YandexGame.RewardVideoEvent -= x
+				)
+				.Subscribe( code => Rewarded.Execute( (ERewardedType)code ) )
+				.AddTo( this );
+		}
+
+		private void InterstitialSubscribe()
+		{
+			ObserveEvent( YandexGame.OpenFullAdEvent, () => AdOpened.Execute( EAdType.Interstitial ) );
+			ObserveEvent( YandexGame.ErrorFullAdEvent, () => AdShowFailed.Execute( EAdType.Interstitial ) );
+			ObserveEvent( YandexGame.CloseFullAdEvent, () =>
+			{
+				AdClosed.Execute( EAdType.Interstitial );
+				AdLoaded.Execute( EAdType.Interstitial );
+			} );
+		}
+
+		private void InitializeSubscribe() =>
+			ObserveEvent( YandexGame.GetDataEvent, () => OnInit() );
+
+		private void OnInit()
+		{
+			Initialized.Execute();
+			AdLoaded.Execute( EAdType.Banner );
+			AdLoaded.Execute( EAdType.RewardedVideo );
+			AdLoaded.Execute( EAdType.Interstitial );
+		}
+
+#region IAdsProvider
+
+		public ReactiveCommand<EAdType> AdLoaded { get; } = new();
+
+		public ReactiveCommand<EAdType> AdOpened { get; } = new();
+
+		public ReactiveCommand<EAdType> AdClosed { get; } = new();
+
+		public ReactiveCommand<EAdType> AdShowFailed { get; } = new();
+
+		public ReactiveCommand Initialized { get; } = new();
+
+		public ReactiveCommand<ERewardedType> Rewarded { get; } = new();
+
+		public void DisplayBanner( string place ) =>
+			YandexGame.StickyAdActivity( true );
+
+		public void HideBanner() =>
+			YandexGame.StickyAdActivity( false );
+
+		public bool IsAdAvailable( EAdType type ) => true;
+
+		public void ShowInterstitialVideo( string place ) =>
+			YandexGame.FullscreenShow();
+
+		public void ShowRevardedVideo( ERewardedType type ) =>
+			YandexGame.RewVideoShow( (int)type );
+
+#endregion
+
+		private void ObserveEvent(Action action, Action callback)
+		{
+			Observable.FromEvent(
+					x => action += x,
+					x => action -= x
+				)
+				.Subscribe( _ => callback.Invoke() )
+				.AddTo( this );
+		}
+
+	}
+}
