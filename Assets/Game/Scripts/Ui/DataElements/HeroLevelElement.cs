f70c66a7ff4678e1d1c2b9c8d22a76fc3cd71028
Erzyukov <erzyukov@gmail.com>
2024-03-19 06:48:23 +1000

    refact

--- Assets/Game/Scripts/Ui/DataElements/HeroLevelElement.cs
+++ Assets/Game/Scripts/Ui/DataElements/HeroLevelElement.cs
@@ -14,10 +14,7 @@ namespace Game.Ui
 		[SerializeField] private TextMeshProUGUI _expiriencePercent;
 
 		[Header("Low Hero Level Alert")]
-		[SerializeField] private Image _backgroun;
-		[SerializeField] private Color _alertColor;
-		[SerializeField] private float _alertDuration;
-		[SerializeField] private int _alertRepeats;
+		[SerializeField] private ImageAlertInput _alert;
 
 		[Inject] private IGameHero _gameHero;
 		[Inject] private IResourceEvents _resourceEvents;
@@ -51,8 +48,8 @@ namespace Game.Ui
 		{
 			_alertTween?.Rewind();
 
-			_alertTween = _backgroun.DOColor( _alertColor, _alertDuration )
-				.SetLoops( _alertRepeats, LoopType.Yoyo )
+			_alertTween = _alert.Image.DOColor( _alert.Color, _alert.Duration )
+				.SetLoops( _alert.Repeats, LoopType.Yoyo )
 				.SetEase( Ease.InOutSine );
 		}
 	}
---
+++ Assets/Game/Scripts/Ui/Hud/ImageAlertInput.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Ui
+{
+	using System;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	[Serializable]
+	public struct ImageAlertInput
+	{
+		public Image Image;
+		public Color Color;
+		public float Duration;
+		public int Repeats;
+	}
+}
