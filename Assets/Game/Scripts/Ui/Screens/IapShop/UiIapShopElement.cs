b7006e9115fd16009c8929b467fae73cf7223415
Erzyukov Alexey <erzyukov@gmail.com>
2024-03-30 21:36:02 +1000

    ui iap shop screen

--- Assets/Game/Scripts/Ui/Screens/IapShop/UiIapShopElement.cs
+++ Assets/Game/Scripts/Ui/Screens/IapShop/UiIapShopElement.cs
@@ -11,9 +11,21 @@ namespace Game.Ui
 		[SerializeField] private EIapProduct _iapProduct;
 		[SerializeField] private TextMeshProUGUI _price;
 		[SerializeField] private Button _button;
+		[SerializeField] private GameObject _activeState;
+		[SerializeField] private GameObject _soldState;
 
 		public EIapProduct IapProduct => _iapProduct;
-		public void SetPrice( string value ) => _price.text = value;
+
+		public void SetPrice( string value ) =>
+			_price.text = value;
+
+		public void SetBought()
+		{
+			_activeState.SetActive( false );
+			_soldState.SetActive( true );
+			_button.interactable = false;
+		}
+
 		public IObservable<Unit> Clicked => _button.OnClickAsObservable();
 	}
 }
--- Assets/Game/Scripts/Ui/Screens/IapShop/UiIapShopPresenter.cs
+++ Assets/Game/Scripts/Ui/Screens/IapShop/UiIapShopPresenter.cs
@@ -3,16 +3,53 @@
 	using Game.Profiles;
 	using Game.Utilities;
 	using Zenject;
+	using UniRx;
+	using Game.Iap;
+	using System.Linq;
 
 	public class UiIapShopPresenter : ControllerBase, IInitializable
 	{
 		[Inject] private IUiIapShopScreen _screen;
 		[Inject] private GameProfile _gameProfile;
+		[Inject] private IIapFacade _iapFacade;
+
+		IapShopProfile Profile => _gameProfile.IapShopProfile;
 
 		public void Initialize()
 		{
-			
+			_screen.Products.ForEach( product => InitProductElement( product ) );
+
+			_iapFacade.OnBoughtOrRestored
+				.Subscribe( OnProductBought )
+				.AddTo( this );
 		}
 
+		private void OnProductBought( EIapProduct product )
+		{
+			var productElement = _screen.Products.Where( e => e.IapProduct == product ).FirstOrDefault();
+
+			if (productElement != null)
+				productElement.SetBought();
+		}
+
+		private void InitProductElement( UiIapShopElement product )
+		{
+			if (Profile.BoughtProducts.Contains( product.IapProduct ))
+			{
+				product.SetBought();
+			}
+			else
+			{
+				product.SetPrice( _iapFacade.GetLocalizedPrice( product.IapProduct ) );
+				product.Clicked
+					.Subscribe( _ => OnProductBuy(product.IapProduct))
+					.AddTo( this );
+			}
+		}
+
+		private void OnProductBuy( EIapProduct iapProduct )
+		{
+			_iapFacade.Buy( iapProduct );
+		}
 	}
 }
