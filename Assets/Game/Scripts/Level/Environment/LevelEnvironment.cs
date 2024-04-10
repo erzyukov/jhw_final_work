6d3a8bd98a216e2b82a2c803e53b3db3287c1895
Erzyukov Alexey <erzyukov@gmail.com>
2024-04-11 07:42:25 +1000

    set level environment on start level

--- Assets/Game/Scripts/Level/Environment/LevelEnvironment.cs
+++ Assets/Game/Scripts/Level/Environment/LevelEnvironment.cs
@@ -1,21 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Game
+namespace Game.Level
 {
-    public class LevelEnvironment : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+	using Game.Configs;
+	using Game.Profiles;
+	using UnityEngine;
+	using Zenject;
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+	public class LevelEnvironment : MonoBehaviour
+    {
+		[SerializeField] Renderer _groundRenderer;
+
+		[Inject] GameProfile _gameProfile;
+		[Inject] LevelsConfig _config;
+
+		private void Start()
+		{
+			int index = _gameProfile.LevelNumber.Value - 1;
+			Sprite environment = _config.Levels[index].Environment;
+			_groundRenderer.material.SetTexture( "_MainTex", environment.texture );
+		}
+
+	}
 }
