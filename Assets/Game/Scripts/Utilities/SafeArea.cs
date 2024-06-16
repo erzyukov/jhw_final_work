4c75c46c7e406f7f8fbbe84f7e500500174ef478
Erzyukov Alexey <erzyukov@gmail.com>
2024-06-16 14:29:59 +1000

    setup safe zone

---
+++ Assets/Game/Scripts/Utilities/SafeArea.cs
@@ -0,0 +1,39 @@
+﻿namespace Game.Utilities
+{
+	using UnityEngine;
+
+
+	public class SafeArea : MonoBehaviour
+	{
+		RectTransform panel;
+		Rect safeAreaRect;
+
+		void Start()
+		{
+			panel = GetComponent<RectTransform>();
+
+			if (panel == null)
+			{
+				Debug.LogError("Cannot apply safe area - no RectTransform found on " + name);
+				Destroy(gameObject);
+			}
+
+			safeAreaRect = Screen.safeArea;
+			ApplySafeArea(safeAreaRect);
+		}
+
+		void ApplySafeArea( Rect r )
+		{
+			Vector2 anchorMin		= r.position;
+			Vector2 anchorMax		= r.position + r.size;
+			anchorMin.x				/= Screen.width;
+			anchorMin.y				/= Screen.height;
+			anchorMax.x				/= Screen.width;
+			anchorMax.y				/= Screen.height;
+			panel.anchorMin			= anchorMin;
+			panel.anchorMax			= anchorMax;
+
+			// Debug.LogFormat("New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}", name, r.x, r.y, r.width, r.height, Screen.width, Screen.height);
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Utilities/SafeBottomPanel.cs
@@ -0,0 +1,19 @@
+﻿namespace Game.Utilities
+{
+	using UnityEngine;
+
+
+	public class SafeBottomPanel : MonoBehaviour
+	{
+		void Start()
+		{
+			float screenHeight		= Screen.height;
+			Rect safeArea			= Screen.safeArea;
+			RectTransform canvas	= GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+			RectTransform panel		= GetComponent<RectTransform>();
+
+			panel.sizeDelta			+= canvas.sizeDelta.y * safeArea.yMin / screenHeight * Vector2.up;
+		}
+	}
+}
+
